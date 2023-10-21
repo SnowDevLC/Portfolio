8437d2eac68ca88db64ffbd16a6c439834e69b12
Luis Canales <canalesluis9@gmail.com>
2023-10-21 11:31:58 -0300

    fix(navBar): add popup menu mobile

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,83 @@
-import { BsArrowUpRight } from 'react-icons/bs';
-import style from './NavBar.module.css';
+import { BsArrowUpRight, BsFileCodeFill } from "react-icons/bs";
+import { FiLink } from "react-icons/fi";
+import { VscChromeClose } from "react-icons/vsc";
+import style from "./NavBar.module.css";
+import { useState } from "react";
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className={style.container}>
-            <div className={style.logoText}>
-                <h2 className={style.textLogo}>Luis Canales</h2>
-                <h3 className={style.underTextLogo}>FullStack Developer</h3>
+        <>
+            <div className={style.sectionNav}>
+                <div className={style.navBar}>
+                    <div className={style.navLeft}>
+                        <div className={style.logoBlock}>
+                            <BsFileCodeFill className={style.logo} />
+                            <div className={style.logoText}>
+                                <h2 className={style.textLogo}>Luis Canales</h2>
+                                <h3 className={style.underTextLogo}>FullStack Developer</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.navMiddle}>
+                        <div className={style.navButtons}>
+                            <a className={style.navToggle} href="/">
+                                Work
+                            </a>
+                            <a className={style.navToggle} href="/info">
+                                Info
+                            </a>
+                        </div>
+                    </div>
+                    <div className={style.navRight}>
+                        <div className={style.socialsNav}>
+                            <a className={style.social} href="https://www.linkedin.com/in/canalesluis9/" target="blank">
+                                LinkedIn
+                                <BsArrowUpRight />
+                            </a>
+                            <a className={style.social} href="https://github.com/SnowDevLC" target="blank">
+                                GitHub
+                                <BsArrowUpRight />
+                            </a>
+                            <a
+                                className={style.social}
+                                href="https://drive.google.com/file/d/1HI6tna5YOVspn_lPmQGycFrLtUzFVNDr/view?usp=share_link"
+                                target="blank"
+                            >
+                                CV
+                                <BsArrowUpRight />
+                            </a>
+                        </div>
+                        <a
+                            href="#"
+                            className={`${style.menuSocial} ${isOpen && style.open}`}
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            <FiLink size={28} className={`${style.popupOpen} ${isOpen && style.open}`} />
+                            <VscChromeClose size={30} className={`${style.popupClose} ${isOpen && style.open}`} />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className={style.nav}>
-              <a className={style.navToggle} href="/">Work</a>
-              <a className={style.navToggle} href="/info">Info</a>
+            <div className={`${style.popupMenu} ${isOpen && style.open}`}>
+                <a className={style.socialMenu} href="https://www.linkedin.com/in/canalesluis9/" target="blank">
+                    LinkedIn
+                    <BsArrowUpRight />
+                </a>
+                <a className={style.socialMenu} href="https://github.com/SnowDevLC" target="blank">
+                    GitHub
+                    <BsArrowUpRight />
+                </a>
+                <a
+                    className={style.socialMenu}
+                    href="https://drive.google.com/file/d/1HI6tna5YOVspn_lPmQGycFrLtUzFVNDr/view?usp=share_link"
+                    target="blank"
+                >
+                    CV
+                    <BsArrowUpRight />
+                </a>
             </div>
-            <div className={style.social}>
-              <a className={style.socialNav} href="https://www.linkedin.com/in/canalesluis9/">LinkedIn<BsArrowUpRight /></a>
-              <a className={style.socialNav} href="https://github.com/SnowDevLC">GitHub<BsArrowUpRight /></a>
-              <a className={style.socialNav} href="https://drive.google.com/file/d/1HI6tna5YOVspn_lPmQGycFrLtUzFVNDr/view?usp=share_link" target='blank'>CV<BsArrowUpRight /></a>
-            </div>
-        </div>
+        </>
     );
 }
